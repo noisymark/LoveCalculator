0a3997ac82f45e33df7d99a7f9f6d0f435625b96
Marko Pavlović <markopavlovic316@gmail.com>
2023-01-28 03:41:11 +0100

    Importing Bootstrap and basic index php template from my previous project :airplane:

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once 'components/header.php'?>
+<body>
+<div id="uvodnitext" class="row text-white text-center">
+    <h4>Welcome to the Love Calculator</h4>
+</div>   
+
+<div class="container">
+    <div class="row text-center">
+        <div class="col-xs-6 col-md-6 col-xl-6">
+            <img src="components/img/heart.png" alt="" height="70%" width="70%">
+        </div>
+        <div class="col-xs-6 col-md-6 col-xl-6">
+            <img src="components/img/heart.png" alt="" height="70%" width="70%">
+        </div>
+    </div>
+</div>
+
+
+<?php include_once 'components/footer.php'?>
+</body>
+<?php include_once 'components/scripts.php'?>
+</html>
